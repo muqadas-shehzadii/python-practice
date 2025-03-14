4ef6b9731bcd5a499149bb6fd55520b32c42df9d
Muqadas Shehzadi <muqadasshahzedi@gmail.com>
2025-03-14 19:21:45 +0000

    uploaded
    I have uploaded my fisrt revision code file here

---
+++ revision.py
@@ -0,0 +1,80 @@
+print('Hello World')
+var = "muqadas"
+my_var = "muqadas"
+_my_var = "my name is muqadas shehzadi"
+myVar = "my name is muqadas shehzadi , i read in 8th smester"
+myvar2 = "hi I am here to asist myself"
+
+print(var)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(myvar2)
+
+x , y , z = "orange" , "banana ","cherry"
+print(x)
+print(y)
+print(z)
+
+
+num = 5
+num2 = "string"
+num3 = 4.5
+num4 = True
+num5 = 45692154653634
+num6 = -77837
+num7 = 12E4
+print(type(num))
+print(type(num2))
+print(type(num3))
+print(type(num4))
+print(type(num5))
+print(type(num6))
+print(type(num7))
+
+var = "Hello World"
+print(var)
+
+var1 = """
+       my name is muqadas shehzadi I am from shiekhupura.
+       I am student at Govt.Graduate college Shiekhupura
+       ppursuing a Bs in IT.
+      """
+print(var1)
+
+name1 = "Hello world"
+print(name1[:5])
+print(name1[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+string_0 = "Myself Muqadas and I am saying "
+string = " Hello , world "
+print(string.upper())
+print(string.upper()[:6])
+print(string.lower())
+print(string.split())
+print(string.strip())
+
+print(string.replace('H', 'M'))
+print(string_0 + string)
+print(f'{string_0}{string}')
+
+print(string.encode())
+
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+
+
+
+
+
+
