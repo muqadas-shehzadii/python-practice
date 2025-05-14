1e90b69e3e394af18479b4848ad35780a154b1f6
Muqadas Shehzadi <muqadasshahzedi@gmail.com>
2025-05-14 10:48:01 -0700

    done
    if-statements

---
+++ codelogic.py
@@ -0,0 +1,54 @@
+# age = int(input("Enter Age:"))
+# day = "Wednesday"
+# price = 12 if age>=18 else 8
+# if day == "Wednesday":
+#     price = price-2
+#     print("Your ticket price is $:" , price)
+
+
+# grade = int(input("Enter your score:"))
+# if grade >=101:
+#     print("Enter valid score")
+#     exit()
+# if grade >=90:
+#     print("Your grade is A")
+# elif grade >=80:
+#     print("Your grade is B") 
+# elif grade >=70:
+#     print("Your grade is B+")
+# else:
+#     print("You are below everage")       
+
+# fruit = input("Enter a fruit name: ")
+# ripeness = input("Enter Ripeness: ")
+
+# if ripeness.lower() == "green":
+#     print("Fruit is unripe")
+# elif ripeness.lower() == "yellow":
+#     print("Fruit is ripe")  
+# elif ripeness.lower() == "brown":
+#     print("Fruit is overripe")   
+# else:
+#     print("Ripeness not found")
+
+weather = input("Enter Weather: ").lower()
+if weather == 'sunny':
+    print("Go for a walk")
+elif weather == 'rainy':
+    print("Read a book")
+elif weather == 'snowy':
+    print("Build a snowman")
+else:
+    print("No weather found") 
+
+# weather = input("Enter Weather: ").lower()
+
+# if weather == 'sunny':
+#     print("Go for a walk")
+# elif weather == 'rainy':
+#     print("Read a book")
+# elif weather == 'snowy':
+#     print("Build a snowman")
+# else:
+#     print("No weather found")
+
